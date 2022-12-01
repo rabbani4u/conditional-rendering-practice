e7cacdbf90d6c6e0149975e8ea4e8eb4f6308442
rabbani4u <rabbani.iu31@gmail.com>
2022-12-02 00:05:10 +0600

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import Form from "./Form";
-import Registration from "./Registration";
-import Login from "./Login";
 
-var userIsRegistered = true;
-
-function createLoggedin() {
-  if (userIsRegistered === false) {
-    return <Form />;
-  } else {
-    return <Login />;
-  }
-}
+var userIsRegistered = false;
 
 function App() {
-  return <div className="container">{createLoggedin()}</div>;
+  return (
+    <div className="container">
+      <Form isRegistered={userIsRegistered} />
+    </div>
+  );
 }
 
 export default App;
